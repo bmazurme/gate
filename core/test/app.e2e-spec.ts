2ba3cd28254af95ee94e1b20f17da125b6d958bb
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-06-27 11:36:50 +0300

    add: test

---
+++ core/test/app.e2e-spec.ts
@@ -0,0 +1,22 @@
+import type { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { createTestApp } from './utils/create-test-app';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    app = await createTestApp();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!');
+  });
+});
